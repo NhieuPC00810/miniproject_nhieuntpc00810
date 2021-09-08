acbcc168bba5c88f7fb57f8cbfc706d03648c81e
NhieuPC00810 <nhieuntPC00810@fpt.edu.vn>
2021-09-08 14:19:45 +0700

    delete

--- src/main/java/com/example/demo/dao/OrderDetailDAO.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.demo.entity.OrderDetail;
-
-public interface OrderDetailDAO extends JpaRepository<OrderDetail, Long> {
-
-}
